eff5419e71c84252e8bdacabd79db448c1a3d937
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2025-12-04 09:39:09 -0300

    Update urls.py

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from django.views.generic import RedirectView # Importação necessária!
+from django.views.generic import RedirectView 
 from . import views
 
 urlpatterns = [
-    # 1. Rota de Redirecionamento: Mapeia o caminho vazio ('/') para /upload/
     path('', RedirectView.as_view(url='upload/', permanent=True), name='home_redirect'),
 
-    # 2. Rota de Upload (agora acessível via /upload/)
-    path('upload/', views.process_uploaded_pdf_api, name='upload_api_root'), 
-    
-    # 3. Rota de Perguntas
-    path('ask/', views.ask_rag_api, name='ask_rag_api'),
+    path('upload/', views.upload_interface_and_process, name='upload_interface'), 
+
+    path('ask/', views.ask_rag_api, name='ask_rag'),
 ]
