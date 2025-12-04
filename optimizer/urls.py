30347f878b0ec7c70d9e0b7fc90df537e26c5816
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2025-12-04 09:50:14 -0300

    Update urls.py

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -3,9 +3,13 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    # Mapeia a URL vazia ('/') para redirecionar para 'upload/'
     path('', RedirectView.as_view(url='upload/', permanent=True), name='home_redirect'),
 
+    # Mapeia '/upload/' para a view que renderiza o HTML e lida com o POST do formulário
+    # O nome (name) 'upload_interface' corresponde ao uso no template HTML.
     path('upload/', views.upload_interface_and_process, name='upload_interface'), 
 
+    # Mapeia '/ask/' para a view de API que lida com as perguntas da IA
     path('ask/', views.ask_rag_api, name='ask_rag'),
 ]
